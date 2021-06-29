044699b619041156fda998c9fc1abf0fb676a5cf
shihar <shiharbinsideeque73@gmail.com>
2021-06-29 17:51:54 +0530

    get youtube videos

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import *
+from youtube import dw_YouTube
 import time
+import os
 
 
+    
 app = Flask(__name__)
 app.secret_key = "dont tell"
 
@@ -15,15 +18,26 @@ def youtube():
     if request.method == 'POST':
         url = request.form["url"]
         quality = request.form["quality"]
-        if url[:23] == "https://www.youtube.com" and len(url) > 23:
-            print(url, quality)
-            print(len(url))
-            return redirect("/youtube")
-        else:
-            flash('Pleas check URL')
-            return redirect(url_for("youtube"))
+        if (url[:23] == "https://www.youtube.com" or url[:16] == "https://youtu.be")  and len(url) > 23 :
+            path = current_app.root_path + "/static/youtube_videos"
 
+            youtube = dw_YouTube(url, quality, path)
+            msg = youtube.download_video()
+            print(msg)
+            if msg == "Error":
+                print('Download Error Pleas check URL')
+                flash('Download Error Pleas check URL', "err")
+                return redirect(url_for("youtube"))
+            else : 
+                flash(msg, "data")
+                return redirect("/youtube")
+
+        else:
+            flash('Pleas check URL', "err")
+            return redirect(url_for("youtube"))
+  
     return render_template('youtube.html')
+
 
 @app.route("/insta")
 def insta():
@@ -32,4 +46,4 @@ def insta():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True)
--- youtube.py
+++ youtube.py
@@ -1,25 +1,47 @@
 from pytube import YouTube
 
 
-class YouTube:
+class dw_YouTube:
+    err = None
     def __init__(self, url, quality, name):
         self.url = url
         self.quality = quality
         self.name = name
-    
+
 
 
     def download_video(self):
-        choice = self.quality
-        url = self.url
-        yt = YouTube(url)
+        print(self.url, self.quality, self.name)
+        try:
+            choice = self.quality
+            url = self.url
+            yt = YouTube(url)
+            if choice == 1 :
+                select = yt.streams.filter(progressive=True).first()
+            elif choice == 0 or 2:
+                select = yt.streams.filter(progressive=True, file_extension="mp4").last()
+            elif choice == 3:
+                select = yt.streams.filter(only_audio=True).first()
 
-        if choice == 1 :
-            select = yt.streams.filter(progressive=True).first()
-        elif choice == 0 or 2:
-            select = yt.streams.filter(progressive=True, file_extension="mp4").last()
-        elif choice == 3:
-            select = yt.streams.filter(only_audio=True).first()
+            # download function
+            # select.download(self.name)
+            self.msg = yt.title
+            print("download seccess", self.msg)
+            return self.msg
+        except:
+            print("Error downloading")
+            self.msg= "Error"
+            return self.msg
 
-        # download function
-        select.download(self.name)
+
+
+def main():
+    url = input("enter url : ")
+    c= 0
+    path = "/root/Documents/web/web-develop/develop/downloader-flask/static/youtube_videos" 
+    youtube =dw_YouTube(url, c, path)
+    youtube.download_video()
+
+
+if __name__ == "__main__":
+    main()
